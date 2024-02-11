fa758b2490ab842bba147bd70cf2d3c4f9585c44
BADIUDragos <dragos.badiu.a@hotmail.com>
2024-02-11 13:44:17 -0500

    clean

--- frontend/src/router/admin/routes.tsx
+++ frontend/src/router/admin/routes.tsx
@@ -8,8 +8,6 @@ const adminRoutes = {
     adminProductRoutes,
     adminUsersRoutes,
     adminOrdersRoutes
-    // { path: "users", element: <AdminUsersPage/> },
-    // { path: "orders", element: <AdminOrdersPage/> }
   ],
 };
 export default adminRoutes;
