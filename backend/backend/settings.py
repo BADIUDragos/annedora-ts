19967896f3eec79b84b99651e80b9b2f82bdb8d5
BADIUDragos <dragos.badiu.a@hotmail.com>
2024-06-11 15:23:31 -0400

    preview pic in edit product works

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,7 +70,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=6),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
